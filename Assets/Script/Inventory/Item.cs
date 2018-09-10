0de707e4718c239509f5eb4ac3ebbfb57d2ff901
graceright <parkhj951109@gmail.com>
2018-09-11 00:20:22 +0900

    Revert "Merge branch 'master' of https://github.com/songhyunji/Dreamcatcher"
    This reverts commit cb21cacb2c009dea62792bef9a0b90786a5d6454, reversing
    changes made to beab615ecbb1a597587a80ac7705d7ac9c68ae65.

--- Assets/Script/Inventory/Item.cs
+++ Assets/Script/Inventory/Item.cs
@@ -1,7 +1,37 @@
-﻿public class Item{
-    public int id { get; set; }  
-    public string name { get; set; }
-    public string description { get; set; }
-    public int activated { get; set; }                // 0 : 해금안된 상태 1 : 해금된 상태 + 사용 가능 2 : 해금된 상태 + 사용 불가능
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+
+public class Item : MonoBehaviour {
+
+    GameObject item_Icon;
+    GameObject textPanel;
+
+    public int itemID;
+    public string itemName;
+    public string itemDescription;
+    public bool isActivated;
+
+
+
+	void Start () {
+        item_Icon = transform.Find("Item_Icon").gameObject;
+        textPanel = transform.parent.parent.Find("Text_Panel").gameObject;
+        //database에서 isActivated 값을 받아옴
+	}
+	
+    public void ItemActivated()
+    {   //inventory의 activatedItem에 의해 실행됩니다.
+        item_Icon.SetActive(true);
+        isActivated = true;
+        //database에 isActivated 값을 반영함
+    }
+
+    public void pressSlotBtn()
+    {//해당 슬롯을 누르면 알맞은 아이템 이름과 설명이 text에 덮어씌워집니다. 역시 Button의 OnClick() 실행 메소드입니다.
+        textPanel.transform.GetChild(0).GetComponent<Text>().text = itemName;
+        textPanel.transform.GetChild(1).GetComponent<Text>().text = itemDescription;
+    }
 }
