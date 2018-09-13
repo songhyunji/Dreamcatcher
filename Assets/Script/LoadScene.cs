3fdce8cc429710fb86fa75aa80a63d56aeb3e358
graceright <parkhj951109@gmail.com>
2018-09-13 15:53:52 +0900

    맵 A 구현 완료
    * 사실 A2맵이 좀 맛탱이가 갔는데 지금은 잘 플레이 됩니다 (ㅠㅠ) 시간이 없어서 이번주는 일단 이렇게 올립니다.
    * 테스트때문에 점프가 엄청 높습니다
    문제점
    1. 사선방향 맵에서의 부자연스러운 움직임 - 캐릭터 스크립트에 추가 필요
    2. one-way collision을 위한 platform collider가 지금은 작동하지 않습니다. 제가 적용해놓은 플레이어 스크립트가 믈리법칙을 개조한 스크립트라 작동을 안해요. 저희가 만든 스크립트 넣으면 정상작동할듯

---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+public class LoadScene : MonoBehaviour {
+
+    public string sceneName;
+    public int sceneNum;
+    public CinemachineVirtualCamera vcam;
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(sceneName + sceneNum);
+    }
+}
---
+++ Assets/Script/VcamSceneLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VcamSceneLoad : MonoBehaviour {
+
+    Cinemachine.CinemachineVirtualCamera vcam;
+
+	void Start () {
+        vcam = GameObject.Find("Camera").transform.GetChild(1).GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        vcam.Follow = GameObject.Find("TestPlayer").transform;
+    }
+}
