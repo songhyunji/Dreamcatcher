1d9b8155c2286618326c9082884244a268475afd
songhyunji <37237094+songhyunji@users.noreply.github.com>
2018-09-15 02:25:01 +0900

    Modify Player Script + Collider (+ Tiles......)
    1. 플레이어 스크립트를 수정하였습니다. (좀 더 부드럽게, Slope 잘 올라갈 수 있게)
    2. Stage A에 UI를 삽입하였습니다. (jump btn, interact btn)
    3. Slope 타일을 일반 오브젝트로 교체하였습니다. (콜라이더 문제) 추후 타일을 다시 그려야할지도 ...ㅠ

---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+public class LoadScene : MonoBehaviour {
+
+    public string sceneName;
+    public int sceneNum;
+    public CinemachineVirtualCamera vcam;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Touch Load Scene Collider");
+            SceneManager.LoadScene(sceneName + sceneNum);
+        }
+    }
+}
---
+++ Assets/Script/Test/Player.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+	public float jumpHeight = 4;
+	public float timeToJumpApex = .4f;
+	float accelerationTimeAirborne = .2f;
+	float accelerationTimeGrounded = .1f;
+	float moveSpeed = 6;
+
+	float gravity;
+	float jumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	private bool isJumping = false;
+	public float dir;
+	public Vector2 startPos;			
+	public Vector2 direction;
+
+	Controller2D controller;
+	
+	void Awake () 
+	{
+
+		DontDestroyOnLoad(this);
+		if (FindObjectsOfType(GetType()).Length > 1)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void Start() {
+		controller = GetComponent<Controller2D> ();
+
+		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+	}
+
+	void Update() {
+
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+		
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			if (touch.position.x <= Screen.width / 2)
+			{
+				switch (touch.phase)
+				{
+					case TouchPhase.Began:
+						startPos = touch.position;
+						break;
+
+					case TouchPhase.Moved:
+						direction = touch.position - startPos;
+						if (direction.x > 0)
+						{
+							dir = 1;
+						}
+						else if (direction.x < 0)
+						{
+							dir = -1;
+						}
+
+						break;
+
+					case TouchPhase.Ended:
+						dir = 0;
+						break;
+				}
+			}
+			else
+			{
+				dir = 0;
+			}
+		}
+		
+		Vector2 input = new Vector2(dir, Input.GetAxisRaw("Vertical"));
+		//print(input.x);
+		
+
+		if (isJumping)
+		{
+			velocity.y = jumpVelocity;
+			isJumping = false;
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+	}
+
+	public void PressJumpBtn()
+	{
+		if (controller.collisions.below)
+		{
+			isJumping = true;
+		}
+		else
+		{
+			isJumping = false;
+		}
+	}
+
+}
