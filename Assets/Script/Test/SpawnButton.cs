2d54fc35e0c8d015422f8fdf49049f05a6356222
songhyunji <37237094+songhyunji@users.noreply.github.com>
2018-11-10 15:57:56 +0900

    Add wolf (stand)
    스폰 버튼을 누르면 늑대를 플레이어 위치에 소환하고 발판 역할을 할 수 있도록 구현했습니다.

---
+++ Assets/Script/Test/SpawnButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SpawnButton : MonoBehaviour, IPointerDownHandler
+{
+
+	public GameObject player;
+	public GameObject wolf;
+
+	private Renderer player_renderer;
+	private GameObject wolf_clone;
+	private bool isSpawned = false;
+
+	private void Start()
+	{
+		player_renderer = player.GetComponent<Renderer>();
+	}
+
+	public void OnPointerDown(PointerEventData ped)
+	{
+		if (!isSpawned)
+		{
+			wolf_clone = Instantiate(wolf, player_renderer.bounds.center, Quaternion.identity);
+			isSpawned = true;
+
+		}
+		else
+		{
+			Destroy(wolf_clone);
+			isSpawned = false;
+
+		}
+	}
+	
+}
