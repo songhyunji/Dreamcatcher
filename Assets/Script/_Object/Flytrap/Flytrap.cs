e300f7d20c211f8d61ed837d0671a56724aef092
songhyunji <37237094+songhyunji@users.noreply.github.com>
2018-12-26 23:35:54 +0900

    Modify main & Add Flytrap object
    1. 메인 씬 UI 수정하였습니다.
    2. 파리지옥 오브젝트 구현하였습니다.
    
    파리지옥 오브젝트 구현에 관한 것은 기획서 참고 부탁드립니다.

---
+++ Assets/Script/_Object/Flytrap/Flytrap.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Flytrap : MonoBehaviour
+{
+    public bool canEat = true;
+    public GameObject symbol;
+    public GameObject player;
+
+    IEnumerator Eat()
+    {        
+        yield return new WaitForSeconds(5);
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+        player.transform.position = new Vector2(-2,-3.6f);
+        SceneManager.LoadScene(4);
+    }
+
+    IEnumerator CannotEat()
+    {
+        symbol.SetActive(true);
+        canEat = false;
+        yield return null;
+    }
+
+    IEnumerator CanEat()
+    {
+        symbol.SetActive(false);
+        canEat = true;
+        yield return null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")&&canEat)
+        {
+            Debug.Log("Eat");
+            StartCoroutine("Eat");
+        }
+        else if (collision.CompareTag("Foothold"))
+        {
+            Debug.Log("Can not eat");
+            StartCoroutine("CannotEat");
+        }
+
+    }
+    
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Out");
+        }
+        else if (collision.CompareTag("Foothold"))
+        {
+            Debug.Log("Can eat");
+            StartCoroutine("CanEat");
+        }
+    }
+}
---
+++ Assets/Script/_Object/Flytrap/FlytrapSceneLoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlytrapSceneLoad : MonoBehaviour
+{
+
+	public Flytrap FlytrapScript;
+
+	void Start ()
+	{
+		FlytrapScript.player = GameObject.Find("TestPlayer");
+		
+	}
+}
