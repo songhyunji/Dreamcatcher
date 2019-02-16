079efc1f9b7941d53f14d041f144d4d06c866ad1
Bang Seunghee <bsh1162@gmail.com>
2019-02-16 15:27:12 +0900

    맵 이름 사라지기 효과 적용 중
    남은 것
    - 센터/레프트탑 구분하기
    - 폰트, 사이즈 수정

--- Assets/Script/_UI/MapTextUi.cs
+++ Assets/Script/_UI/MapTextUi.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class MapTextUi : MonoBehaviour
 {
-    float speed = 0.1f; // fade out 되는 속도
-    float alpha = 0; // 투명도
+    public float speed = 0.015f; // fade out 되는 속도
+    float alpha = 1.5f; // 투명도
     string currentmap = " "; // 현재 맵 이름
     string currentscene; // 현재 씬 이름
 
@@ -16,22 +16,31 @@ public class MapTextUi : MonoBehaviour
     {
         currentscene = SceneManager.GetActiveScene().name; // 현재 활성화된 씬 이름 불러오기
         //center에 뜨는 ui의 경우 string의 첫 글자를 따서 A면 마녀의 숲, B면 -- 식으로 띄우기
+        DecideMapName();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        DecideMapName();
 
         this.GetComponent<Text>().text = currentmap; // ui text 업데이트
 
-        this.GetComponent<Text>().color = new Color(255, 255, 255, alpha);
+        Debug.Log(this.GetComponent<Text>().color.a);
 
-        if (alpha < 1) // alpha가 1보다 작으면
+        this.GetComponent<Text>().color = new Color(255, 255, 255, alpha);
+        //        this.GetComponent<Text>().color = new Color(255, 255, 255, 1);
+
+        if (alpha <= 1.5 && alpha > -1)
+        { // alpha가 1보다 작고 0보다 크면
+          //(1.5 : 텍스트 유지되는 시간(1~2초), 1.5에서 1.0으로 바뀌면 점점 사라지기 시작함)
             alpha -= speed;
+            Debug.Log(alpha);
+        }
         else
-            alpha = 0;
+//            alpha = 1; // 반복 테스트용
+            alpha = 0; // 사라지기 끝나면 계속 투명한 상태로
+
     }
 
     void DecideMapName() // 맵 이름 정하는 함수
@@ -39,22 +48,28 @@ public class MapTextUi : MonoBehaviour
         switch(currentscene) // 씬 이름에 따라 맵 이름 정함
         {
             case "A1":
-                currentmap = "마녀의 숲 - 1";
+                currentmap = "마녀의 숲 입구 - 1";
                 break;
             case "A2":
-                currentmap = "마녀의 숲 - 2";
+                currentmap = "마녀의 숲 입구 - 2";
                 break;
             case "B1":
-                currentmap = "마녀의 숲 - 2";
+                currentmap = "마녀의 숲 - 1";
                 break;
             case "B2":
                 currentmap = "마녀의 숲 - 2";
                 break;
             case "B3":
-                currentmap = "마녀의 숲 - 2";
+                currentmap = "마녀의 숲 - 3";
                 break;
             case "B4":
-                currentmap = "마녀의 숲 - 2";
+                currentmap = "마녀의 숲 - 4";
+                break;
+            case "B5":
+                currentmap = "마녀의 숲 - 5";
+                break;
+            case "B6":
+                currentmap = "마녀의 숲 - 6";
                 break;
 
         }
