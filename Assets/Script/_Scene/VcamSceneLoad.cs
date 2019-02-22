09cea12f523867555ed19cb16e9ed4155037fc8c
Bang Seunghee <bsh1162@gmail.com>
2019-02-23 00:13:43 +0900

    텍스트 수정 + 카메라 적용 중
    - 폰트 오버플로우 적용 -> 깨짐 없어짐 + 사이즈 커짐!
    - 카메라....?ㅠ

--- Assets/Script/_Scene/VcamSceneLoad.cs
+++ Assets/Script/_Scene/VcamSceneLoad.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VcamSceneLoad : MonoBehaviour {
 
     Cinemachine.CinemachineVirtualCamera vcam;
 
+//    string currentMap; // 현재 맵 이름
+
 	void Start () {
         vcam = GameObject.Find("Camera").transform.GetChild(1).GetComponent<Cinemachine.CinemachineVirtualCamera>();
         vcam.Follow = GameObject.Find("TestPlayer(Clone)").transform;
+    }
+
+    void Update()
+    {
+        
     }
 }
---
+++ Assets/Script/_Scene/VcamSceneLoad2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VcamSceneLoad2 : MonoBehaviour
+{
+    Cinemachine.CinemachineVirtualCamera vcam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vcam = GameObject.Find("Camera").transform.GetChild(2).GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        vcam.Follow = GameObject.Find("Background").transform.GetChild(4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 10000; i++)
+            Debug.Log("잠시 후에");
+
+        //        Destroy(vcam);
+
+        vcam.Priority = 11; // 우선순위 바뀜
+
+        Debug.Log("vcam 파괴");
+
+    }
+}
