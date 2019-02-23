3a18fa103ec9d649bef3a1cc154b5f3b961f9fc5
Bang Seunghee <bsh1162@gmail.com>
2019-02-23 16:30:50 +0900

    프롤로그 폰트 바꿈 + 센터 타이틀 이미지로 변경 중
    - 페이드 아웃 후 타이틀 나타나게 하기

---
+++ Assets/Script/_UI/MapnameImage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapnameImage : MonoBehaviour
+{
+    public float speed = 0.015f; // fade out 되는 속도
+    float alpha = 1.5f; // 투명도
+
+//    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //        this.GetComponent<Image>().color.a = alpha;
+        this.GetComponent<Image>().CrossFadeAlpha(alpha, 0f, true);
+
+        alpha -= speed;
+    }
+}
