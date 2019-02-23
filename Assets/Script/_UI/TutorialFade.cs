eb1fc0e4893e173ae7a5cfefa2c4a3abb8fa9b8b
Bang Seunghee <bsh1162@gmail.com>
2019-02-23 17:19:44 +0900

    튜토리얼 추가
    - 스크린 크기와 !1안 맞음!! 수정해야 함!!1
    - 사이즈를 늘렸더니 이미지 깨짐

---
+++ Assets/Script/_UI/TutorialFade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialFade : MonoBehaviour
+{
+    Vector2 SizeVector = new Vector2(Screen.width, Screen.height);
+
+    float alpha = 1.5f;
+    public float speed = 0.015f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<RectTransform>().sizeDelta = SizeVector; // 스크린 사이즈와 동일하게 사이즈 맞추기
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Image>().CrossFadeAlpha(alpha, 0f, true);
+
+        alpha -= speed;
+    }
+}
